b1329b4cf129b67c5e9eb2ec2a9596c32dcd549e
Caíque Alencar <caiquealeencar@gmail.com>
2019-08-15 20:20:16 -0300

    Exercícios parte 2

---
+++ soma_matrizes.py
@@ -0,0 +1,16 @@
+def soma_matrizes(m1, m2):
+    linhas_m1 = len(m1)
+    linhas_m2 = len(m2)
+    colunas_m1 = len(m1[0])
+    colunas_m2 = len(m2[0])
+    if linhas_m1 != linhas_m2 or colunas_m1 != colunas_m2:
+        return False
+    else:
+        nova_matriz = []
+        for i in range(linhas_m1):
+            linha = []
+            for j in range(colunas_m1):
+                soma = m1[i][j] + m2[i][j]
+                linha.append(soma)
+            nova_matriz.append(linha)
+        return nova_matriz
