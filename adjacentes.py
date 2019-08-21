2e8372755c87263a0b1054208a421879b0bb9a3a
caique-alencar <caiquealeencar@gmail.com>
2019-08-21 16:42:01 -0300

    atualiza

--- adjacentes.py
+++ adjacentes.py
@@ -1,17 +1,17 @@
-n = int(input("Digite um número: "))
+def adjacentes(n):
 
-anterior = -1
-atual = -2
-adjacentes_iguais = False # Indicador de passagem
+    anterior = -1
+    atual = -2
+    adjacentes_iguais = False # Indicador de passagem
 
-while n > 0 and not adjacentes_iguais:
-    atual = n % 10 # Pega o último dígito do número
-    anterior = (n // 10) % 10 # Retira o último dígito do número e pega o anterior
-    if atual == anterior:
-        adjacentes_iguais = True
-    n = n // 10 # Retira o último dígito do número
+    while n > 0 and not adjacentes_iguais:
+        atual = n % 10 # Pega o último dígito do número
+        anterior = (n // 10) % 10 # Retira o último dígito do número e pega o anterior
+        if atual == anterior:
+            adjacentes_iguais = True
+        n = n // 10 # Retira o último dígito do número
 
-if adjacentes_iguais:
-    print("O número tem dois '" + str(atual) + "' como dígitos adjacentes iguais.")
-else:
-    print("O número não possui dígitos adjacentes iguais.")
+    if adjacentes_iguais:
+        print("O número tem dois '" + str(atual) + "' como dígitos adjacentes iguais.")
+    else:
+        print("O número não possui dígitos adjacentes iguais.")
--- area_perimetro_circunferencia.py
+++ area_perimetro_circunferencia.py
@@ -1,13 +1,15 @@
-# Programa para a calcular a área de um círculo e o perímetro de uma circunferência
+# Programa para a calcular a área de um círculo e o
+# perímetro de uma circunferência
 
-r = float(input("Digite o raio do círculo: "))
-pi = 3.14
-pi = float(pi)
-area = pi*(r**2)
-perimetro = 2*pi*r
+def area_perimetro_circunf(r):
 
-print("A área do seu círculo é " + str(area) + ".")
-print("O perímetro da sua circunferência e " + str(perimetro) + ".")
+    pi = 3.14
+    pi = float(pi)
+    area = pi*(r**2)
+    perimetro = 2*pi*r
+
+    print("A área do seu círculo é " + str(area) + ".")
+    print("O perímetro da sua circunferência e " + str(perimetro) + ".")
 
 
 
